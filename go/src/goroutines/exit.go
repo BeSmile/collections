4046b627cdeba63b6eb57487b0590d5f158e2ce1
BeSmile <l735465519@gmail.com>
2018-11-19 17:50:23 +0800

    :sparkless: goroutines退出

---
+++ go/src/goroutines/exit.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+//在多个子goroutine中触发事件的最简单方法是提供一个在准备就绪时关闭的go-chan。该go-chan上的所有未决接收将激活，而不是必须向每个goroutine发送单独的信号。
+func main() {
+	exitChan := make(chan int)
+	go task1(exitChan)
+	go task2(exitChan)
+	time.Sleep(2 * time.Second)
+	close(exitChan)
+}
+
+func task1(exitChan chan int) {
+	//<-exitChan
+	log.Printf("task1 exiting")
+}
+
+func task2(exitChan chan int) {
+	<-exitChan
+	log.Printf("task2 exiting")
+}
