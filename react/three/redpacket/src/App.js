a7e7d88790e649aaa73c945e57a2ab3ba90807b9
Smile <l735465519@gmail.com>
2019-12-23 00:48:55 +0800

    feat: 红包掉落

--- react/three/redpacket/src/App.js
+++ react/three/redpacket/src/App.js
@@ -1,57 +1,215 @@
 import React, { useEffect, }  from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as THREE from 'three';
+// import * as THREE from 'three';
 import { Stats } from 'three-stats';
 import _ from 'lodash';
+var THREE = require('three');
+var OBJLoader = require('three-obj-loader');
+OBJLoader(THREE);
+const innerWidth = window.innerWidth;
+
+const innerHeight = window.innerHeight;
+const width = innerWidth / 2 ;
+const height = innerHeight / 2;
+var materialArray = [];
+
+const Eventer = function() {
+  this.pool = [];// 存储池
+  this.subscribers = {};
+  this.max = 10;
+  this.total = 0;
+  this.progress = [];
+  this.init();
+  return {
+    remove: this.remove.bind(this),
+    subscribe: this.subscribe.bind(this),
+    publish: this.publish.bind(this),
+    draw: this.draw.bind(this),
+  }
+}
+
+Eventer.prototype = {
+  constructor: Eventer,
+  // 进程池存在最大数，到达最大数进入存储池
+  doProgress: function(process) {
+    // console.log('push');
+    if (this.pool.length < this.max) {
+      this.pool.push(process);
+    } else {
+      this.progress.push(process);
+    }
+    return this.progress;
+  },
+  init: function() {
+    this.total = 0;
+  },
+  isEmptyProgress: function() {
+    return this.progress.length === 0 ? true : false;
+  },
+  // 发布消息
+  publish: function(trigger, item) {
+    const $this = this;
+    let subscribes = $this.subscribers[trigger];// 获得订阅的事件函数
+    if(!subscribes) return false;
+    $this.doProgress(item);
+    let len = subscribes.length;
+    while(len--) {
+      this.progress.forEach(function(item) {
+        subscribes.reduce(function(result, func) {
+          // console.log('reduce', $this);
+          result = func.call($this, item, result);
+          return result;
+        }, {})
+      })
+    }
+  },
+  // 订阅多个事件
+  subscribe: function(trigger, fn) {
+    if(!this.subscribers[trigger]) {
+      this.subscribers[trigger] = [];
+    }
+    this.subscribers[trigger].push(fn);
+  },
+  // 取消订阅事件
+  unsubscribe: function(trigger) {
+    if(!this.subscribers[trigger]) {
+      return false;
+    }
+    this.subscribers[trigger] = [];
+  },
+  // 删除队列并且
+  remove: function(mesh) {
+    const rmArray = _.remove(this.pool, function(item) {
+      return item.uuid === mesh.uuid;
+    });
+    if (rmArray.length <= 0) {
+      return false;
+    }
+    // TODO: 从存储池获得
+    if (!this.isEmptyProgress()) {
+      this.doProgress(this.progress.shift());
+    }
+    const item = rmArray[0];
+    // 释放geometry
+    item.geometry.dispose();
+    return item;
+  },
+  draw: function(speed = 1) {
+    console.log(this);
+    const $this = this;
+    $this.pool.forEach(function(item) {
+      // 判断是否超出边界 超出边界 删除
+      if (item.position.y < (-height - 64 / 2)) {
+        $this.remove(item);
+      } else {
+        item.position.y -= speed;
+      }
+    })
+  }
+}
+
+// 进度条管理工具
+function initLoadingManager(events) {
+  const manager = new THREE.LoadingManager();
+  for(let name in events) {
+    if (!manager.hasOwnProperty(name)) {
+      throw new Error(`no have function ${name} in LoadingManager `);
+      continue;
+    }
+    manager[name] = events[name];
+  }
+  return new THREE.TextureLoader( manager );
+};
+
+function addZero(a){
+    return ('0' + a).slice(-2);
+}
+
+// 生成红包
+const generateRedPacket = (materialArray) => {
+  const { image } = materialArray[0].map;
+  const { width: imgWidth, height: imgHeight } = image;
+  const ratio = imgWidth / imgHeight;
+  // const ratio = window.devicePixelRatio;
+  const geometry = {
+    x: 64,
+    y: 64 / ratio,
+    ratio: ratio,
+  }
+  var squareGeomery = new THREE.PlaneGeometry(geometry.x, geometry.y, 1);
+
+  var plane = new THREE.Mesh( squareGeomery, materialArray );
+  // const rWdith = _.random(-width + geometry.x, width - geometry.x, 0);
+  const rWdith = _.random(-width + geometry.x / 2, width - geometry.x / 2, 0);
+  plane.position.set(rWdith, height + geometry.y / 2, 0);
+  return plane
+}
 
 function init() {
-  const innerWidth = window.innerWidth;
-  const innerHeight = window.innerHeight;
-  const width = innerWidth / 2 ;
-
-  const height = innerHeight / 2;
+  const event = new Eventer();
+  event.subscribe('push', function(item) {
+    // console.log('push', arguments, this);
+    // event.doProgress(item);
+    return 'push yellow'
+  })
   var stats = initStats();
   var scene = new THREE.Scene();
 
   var axisHelper = new THREE.AxesHelper( 10e3 );
   scene.add( axisHelper );
+
   let camera = new THREE.OrthographicCamera(-width, width, height, -height, 1, 1000);
+
   var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-
   renderer.setSize(window.innerWidth, window.innerHeight);
-  // renderer.setPixelRatio(window.devicePixelRatio)
 
-  var geometry = new THREE.Geometry();
+  var loader = new initLoadingManager  ({
+    onProgress: (item, loaded, total) => {
+      console.log(item, loaded, total);
+      console.log('Loaded:', Math.round(loaded / total * 100, 2) + '%')
+    },
+    onLoad: () => {
+      console.log('Loading complete');
+      console.log(materialArray);
+      for(let i = 0; i < 600; i++) {
+        let plane = generateRedPacket(materialArray);
 
-  for(let i = 0; i < 50; i++) {
-    geometry.vertices.push(
-      new THREE.Vector3( _.random(-width, width),  _.random(-height, height), 0 ),
-    )
+        event.publish('push', plane);
+
+        scene.add( plane );
+      }
+
+      animate();
+    },
+    onError: (url) => {
+      console.log('Error loading');
+    },
+    onStart: (item, loaded, total) => {
+      console.log('Loading started');
+    },
+  });
+
+  // 生成材质包
+  function initMaterial(loader) {
+    Array.from({
+      length: 28,
+    }).map((item, i) => {
+      loader.load( `./images/store_${addZero(i+1)}.png`, function ( texture ) {
+        materialArray[i] = new THREE.MeshBasicMaterial({
+            // color: 0xff0000,
+            map: texture,
+            transparent: true,
+        });
+      } );
+    });
   }
-  // geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
 
-  geometry.computeBoundingSphere();
-  let material = new THREE.MeshLambertMaterial({
-    color: 0x000000,
-  })
-  let mesh = new THREE.Mesh(geometry, material);
+  initMaterial(loader);
 
-  let cube = new THREE.Mesh(new THREE.CubeGeometry(100,100,10), new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-    wireframe:true,
-  }))
-
-  let sprite = new THREE.TextureLoader().load( './smoke.png' );
-
-  let pointsMaterial = new THREE.PointsMaterial({
-    color: 0x000000,
-    size: 120,
-    map: sprite,
-    transparent: true
-  })
-
-  let points = new THREE.Points(geometry, pointsMaterial);
+  var spotLight = new THREE.SpotLight(0xffffff);
+  spotLight.position.set(0, 0, 0);
+  scene.add(spotLight);
 
   var raycaster = new THREE.Raycaster();//光线投射，用于确定鼠标点击位置
 
@@ -62,40 +220,39 @@ function init() {
       //将html坐标系转化为webgl坐标系，并确定鼠标点击位置
       mouse.x =  e.clientX / renderer.domElement.clientWidth*2-1;
       mouse.y =  -(e.clientY / renderer.domElement.clientHeight*2)+1;
-      console.log(mouse);
+      // console.log(mouse);
       //以camera为z坐标，确定所点击物体的3D空间位置
       raycaster.setFromCamera(mouse,camera);
       //确定所点击位置上的物体数量
-      console.log(scene.children);
       var intersects = raycaster.intersectObjects(scene.children);
       //选中后进行的操作
       if(intersects.length){
-          alert(intersects.length)
+        let i = 0;
+        const changematerial = function(i) {
+          if(i < materialArray.length) {
+            setTimeout(function() {
+              intersects[0].object.material = materialArray[i];
+              changematerial(++i);
+            }, 50);
+          } else {
+            const mesh = event.remove(intersects[0].object);
+            scene.remove(mesh)
+          }
+        }
+        changematerial(i);
       }
   }
-
-  // scene.add(cube);
-  // scene.add(mesh);
-  scene.add(points);
 
   camera.position.set(0, 0, 1)
 
   document.body.appendChild(renderer.domElement);
   function animate() {
     stats.update();
-    geometry.vertices.forEach(p => {
-      p.y -= 1.2;
-      // console.log(p.y);
-      if(p.y <= -height) {
-        p.y = height;
-      }
-    })
-    geometry.verticesNeedUpdate = true;
-
+    // plane.position.y -= 1
+    event.draw();
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
   }
-  animate();
 }
 function initStats() {
     var stats = new Stats();
@@ -111,7 +268,9 @@ function initStats() {
     return stats;
 }
 function App() {
-  init();
+  useEffect(function() {
+    init();
+  })
 
   return (
     <></>
