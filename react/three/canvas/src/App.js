7414cf8ac1fdbf680fab55b93af7c64a7e95a09e
BeSmile <l735465519@gmail.com>
2020-01-18 16:02:11 +0800

    feat: 着色器颜色

--- react/three/canvas/src/App.js
+++ react/three/canvas/src/App.js
@@ -12,25 +12,38 @@ var VSHADER_SOURCE =
   'gl_PointSize = 10.0;\n'+
 '}';
 
-var FSHADER_SOURCE = 'void main() {'+
-  'gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);'+
+var FSHADER_SOURCE =
+  'precision mediump float;'+
+  'uniform vec4 u_FragColor;\n' + // uniform变量
+  'void main() {'+
+  'gl_FragColor = u_FragColor;\n'+
 '}';
 var g_points = [];
+var g_colors = [];
 
-function click(ev, gl, canvas, position) {
+function click(ev, gl, canvas, position, color) {
   var x = ev.clientX;
   var y = ev.clientY;
 
   var rect = ev.target.getBoundingClientRect();
   x = ((x - rect.left) - canvas.height/2) / (canvas.height /2);
   y = (canvas.width/2 - (y - rect.top)) / (canvas.width /2);
-  g_points.push(x);
-  g_points.push(y);
+  g_points.push([x, y]);
 
+  if (x >= 0.0 && y >= 0.0) {
+    g_colors.push([1.0, 0.0, 0.0, 1.0])
+  } else if(x < 0.0 && y < 0.0) {
+    g_colors.push([0.0, 1.0, 0.0, 1.0]);
+  } else {
+    g_colors.push([1.0, 1.0, 1.0, 1.0]);
+  }
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  for (var i = 0; i < g_points.length; i+=2) {
-    gl.vertexAttrib3f(position, g_points[i], g_points[i+1], 0.0);
+  for (var i = 0; i < g_points.length; i++) {
+    const xy = g_points[i];
+    const rgba = g_colors[i];
+    gl.vertexAttrib3f(position, xy[0], xy[1], 0.0);
+    gl.uniform4f(color, rgba[0], rgba[1], rgba[2], rgba[3]);
     gl.drawArrays(gl.POINTS, 0, 1);
   }
 }
@@ -54,6 +67,13 @@ function App() {
       console.log('cannot get');
       return;
     }
+
+
+    var color = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (color < 0) {
+      console.log('cannot get color');
+      return;
+    }
     // gl.vertexAttrib1f(position, 1.0);
     // gl.vertexAttrib2f(position, 1.0, 0.0);
     gl.vertexAttrib3f(position, 1.0, 0.0, 0.0);
@@ -65,7 +85,7 @@ function App() {
     gl.drawArrays(gl.Points, 0, 1);
 
     canvas.onmousedown = function(ev) {
-      click(ev, gl, canvas, position)
+      click(ev, gl, canvas, position, color)
     }
   }, [])
   return (
